92b15b1381d18433203e8248552c7b34aa9ca21f
akshatsearce <akshat.prasad@searce.com>
2025-08-21 17:44:32 +0530

    cleanup

--- user_app/app/(dashboard)/notification/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getUserNotifications } from "@/lib/actions/notificationAction";
-import { NEXT_AUTH } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-
-export default async function (){
-    
-}
